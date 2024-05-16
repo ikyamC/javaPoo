f2e7e2400663e776e935b25c28e4b146eb5443fb
unknown <Administrador@MEDAPRCPDFOD035.FormacionSena.RED>
2024-05-16 08:04:08 -0500

    Herencia y Polimorfismo -Implementación del Método calcularSalario() en Clase Gerente por [Nombre]

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,4 +1,16 @@
-public class Gerente  {
+public class Gerente extends Empleado {
+    private double bono;
 
+    public Gerente(String nombre, double salarioBase, double bono) {
+        super(nombre, salarioBase);
+        this.bono = bono;
+    }
+
+    @Override
+    public double calcularSalario() {
+        // El salario del gerente incluye el salario base más el bono
+        return super.calcularSalario() + bono;
+    }
+
+    // Otros métodos, si es necesario
 }
-
