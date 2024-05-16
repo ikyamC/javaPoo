17b370e5783e81d630fea55d02f75c4c674bbd38
unknown <Administrador@MEDAPRCPDFOD035.FormacionSena.RED>
2024-05-16 07:58:59 -0500

    Herencia y Polimorfismo -Implementación de Clase Empleado por [Nombre]

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,1 +1,17 @@
 
+public class Empleado {
+    private String nombre;
+    private double salarioBase;
+
+    public Empleado(String nombre, double salarioBase) {
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+    }
+
+    public double calcularSalario() {
+        // El salario base es el mismo para todos los empleados
+        return salarioBase;
+    }
+
+    // Getters y setters, si es necesario
+}
