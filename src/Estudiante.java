2ec5966efcec504a253c9d3f4051a56b8a72793a
unknown <Administrador@MEDAPRCPDFOD035.FormacionSena.RED>
2024-05-16 07:10:28 -0500

    Herencia - Implementación de Clase Estudiante

---
+++ src/Estudiante.java
@@ -0,0 +1,25 @@
+public class Estudiante extends Persona {
+    private String grado;
+
+    // Constructor
+    public Estudiante(String nombre, int edad, String fechaNacimiento, String grado) {
+        // Llama al constructor de la clase Persona para inicializar las propiedades heredadas
+        super(nombre, edad, fechaNacimiento);
+        this.grado = grado;
+    }
+
+    // Método para obtener el grado del estudiante
+    public String obtenerGrado() {
+        return grado;
+    }
+
+    // Método para establecer el grado del estudiante
+    public void establecerGrado(String grado) {
+        this.grado = grado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Grado: " + grado;
+    }
+}
